7ab6a84281fcdc29bcc96f06315a1f771deea346
caioDeodato <dev.caiolima@gmail.com>
2021-12-26 13:49:12 -0300

    feat: create login screen

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Demo from './Views/Demo'
+import Demo from './Views/Demo';
+import Login from './Views/Login';
 
 function Routes() {
   const { Navigator, Screen } = createNativeStackNavigator();
@@ -10,10 +11,11 @@ function Routes() {
   return (
     <NavigationContainer>
       <Navigator>
-        <Screen name='Demo' component={Demo} options={{  headerShown: false }} />
+        <Screen name="Demo" component={Demo} options={{ headerShown: false }} />
+        <Screen name="Login" component={Login} options={{ headerShown: false }} />
       </Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default Routes;
